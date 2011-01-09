4f4bd645651f0a8bcfe887c990d57999ae9aa76d
John Shahid <jvshahid@gmail.com>
2011-01-08 22:08:19 -0500

    Added another spec to the runtime dependencies specs.

--- spec/runtime_dependencies_spec.rb
+++ spec/runtime_dependencies_spec.rb
@@ -5,10 +5,7 @@ require 'lib/dependency_lock'
 require File.expand_path(File.join('spec', 'spec_helpers'), Gem::GemPathSearcher.new.find('buildr').full_gem_path)
 
 describe 'Runtime dependencies' do
-  it 'should not have duplicate artifacts' do
-    define "TestProject" do
-      extend TransitiveBuildr
-
+  before(:each) do
     write artifact('foo:bar:jar:1.0').pom.to_s, <<-XML
 <project>
   <artifactId>bar</artifactId>
@@ -23,11 +20,36 @@ XML
 </project>
 XML
 
+    write artifact('foo:foobar:jar:1.0').pom.to_s, <<-XML
+<project>
+  <artifactId>foobar</artifactId>
+  <groupId>foo</groupId>
+</project>
+XML
+  end
+
+  it 'should not have duplicate artifacts' do
+    define "TestProject" do
+      extend TransitiveBuildr
+
       run.with 'foo:bar:jar:1.0'
       run.with 'foo:bar:jar:1.1'
     end
 
     expected_runtime_dependencies = [artifact('foo:bar:jar:1.1')]
+    actual_runtime_dependencies = project('TestProject').run.classpath
+    actual_runtime_dependencies.should == expected_runtime_dependencies
+  end
+
+  it 'should have the compile task dependencies' do
+    define "TestProject" do
+      extend TransitiveBuildr
+
+      compile.with 'foo:foobar:jar:1.0'
+      run.with 'foo:bar:jar:1.1'
+    end
+
+    expected_runtime_dependencies = [artifact('foo:foobar:jar:1.0'), artifact('foo:bar:jar:1.1')]
     actual_runtime_dependencies = project('TestProject').run.classpath
     actual_runtime_dependencies.should == expected_runtime_dependencies
   end
