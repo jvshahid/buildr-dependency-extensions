34ed2eeb0120278e248146dac70a80592bedf4f1
John Shahid <jvshahid@gmail.com>
2011-01-14 05:56:24 -0500

    Implemented the transitive dependencies resolution for the test task.

--- lib/transitive-buildr/transitive-buildr.rb
+++ lib/transitive-buildr/transitive-buildr.rb
@@ -6,6 +6,7 @@ module TransitiveBuildr
   include Extension
 
   after_define do |project|
+    # Add the compile dependencies to the run task
     compile_dependencies = project.compile.dependencies.select {|dep| HelperFunctions.is_artifact? dep }
     runtime_dependencies = compile_dependencies + project.run.classpath
     runtime_file_tasks = runtime_dependencies.reject {|dep| HelperFunctions.is_artifact? dep }
@@ -22,5 +23,21 @@ module TransitiveBuildr
       project.artifact(Artifact.to_spec(artifact_hash))
     end
     project.run.classpath = new_runtime_artifacts + runtime_file_tasks
+
+    # Add the compile dependencies to the run task
+    test_dependencies = compile_dependencies + project.test.classpath
+    test_file_tasks = test_dependencies.reject {|dep| HelperFunctions.is_artifact? dep }
+    test_artifacts = test_dependencies - test_file_tasks
+    transitive_test_artifacts = test_artifacts.inject([]) do |set, dependency|
+      set + project.transitive(dependency)
+    end
+    unique_transitive_artifacts = HelperFunctions.get_unique_group_artifact(transitive_test_artifacts)
+    new_test_artifacts = unique_transitive_artifacts.map do |artifact|
+      all_versions = HelperFunctions.get_all_versions artifact, transitive_runtime_artifacts
+      artifact_hash = Artifact.to_hash(artifact)
+      artifact_hash[:version] = version_conflict_resolver.resolve artifact, all_versions
+      project.artifact(Artifact.to_spec(artifact_hash))
+    end
+    project.test.classpath = new_test_artifacts + test_file_tasks
   end
 end
---
+++ spec/test_dependencies_spec.rb
@@ -0,0 +1,71 @@
+require 'rubygems'
+require 'buildr'
+require 'lib/transitive-buildr'
+
+require File.expand_path(File.join('spec', 'spec_helpers'), Gem::GemPathSearcher.new.find('buildr').full_gem_path)
+
+describe 'Test dependencies' do
+  before(:each) do
+    write artifact('foo:bar:jar:1.0').pom.to_s, <<-XML
+<project>
+  <artifactId>bar</artifactId>
+  <groupId>foo</groupId>
+</project>
+XML
+
+    write artifact('foo:bar:jar:1.1').pom.to_s, <<-XML
+<project>
+  <artifactId>bar</artifactId>
+  <groupId>foo</groupId>
+</project>
+XML
+
+    write artifact('foo:foobar:jar:1.0').pom.to_s, <<-XML
+<project>
+  <artifactId>foobar</artifactId>
+  <groupId>foo</groupId>
+</project>
+XML
+  end
+
+  it 'should not have duplicate artifacts' do
+    define "TestProject" do
+      extend TransitiveBuildr
+
+      test.with 'foo:bar:jar:1.0'
+      test.with 'foo:bar:jar:1.1'
+    end
+
+    expected_test_dependencies = [artifact('foo:bar:jar:1.1')]
+    actual_test_dependencies = project('TestProject').test.classpath
+    actual_test_dependencies.should == expected_test_dependencies
+  end
+
+  it 'should have the compile task dependencies' do
+    define "TestProject" do
+      extend TransitiveBuildr
+
+      compile.with 'foo:foobar:jar:1.0'
+      test.with 'foo:bar:jar:1.1'
+    end
+
+    expected_test_dependencies = [artifact('foo:foobar:jar:1.0'), artifact('foo:bar:jar:1.1')]
+    actual_test_dependencies = project('TestProject').test.classpath
+    actual_test_dependencies.should == expected_test_dependencies
+  end
+
+  it 'should not fail when the compile task depends on one or more file tasks' do
+    define "TestProject" do
+      extend TransitiveBuildr
+
+      file _(:target, :classes) do
+      end
+      compile.with file(_(:foo, :bar))
+      test.with 'foo:bar:jar:1.1'
+    end
+
+    expected_test_dependencies = [artifact('foo:bar:jar:1.1'), file(project('TestProject').path_to(:foo, :bar))]
+    actual_test_dependencies = project('TestProject').test.classpath
+    actual_test_dependencies.should == expected_test_dependencies
+  end
+end
