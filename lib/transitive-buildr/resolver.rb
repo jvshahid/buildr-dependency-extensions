e9964c542d813932ce1de49ded2553c019fb5eab
John Shahid <jvshahid@gmail.com>
2011-01-15 19:57:54 -0500

    Removed the interactive conflict resolver.

--- lib/transitive-buildr/resolver.rb
+++ lib/transitive-buildr/resolver.rb
@@ -1,19 +1,4 @@
 module TransitiveBuildr
-  class InteractiveConflictResolver
-    def resolve artifact, all_versions
-      all_versions = all_versions.sort.reverse.uniq
-      selected_version = nil
-      if all_versions.size > 1 then
-        begin
-          puts "Please select the version of #{artifact} that you'd like to use"
-          all_versions.each_index {|index| puts "#{index})\t#{all_versions[index]}"}
-          selected_version_index = $stdin.gets.to_i
-          selected_version = all_versions[selected_version_index] if selected_version_index < all_versions.size
-        end while !selected_version
-      else
-        selected_version = all_versions[0]
-      end
-      selected_version
     end
   end
 
