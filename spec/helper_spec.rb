7ebb76d93ef221d415f14b74af801ebe0bd722fe
John Shahid <jvshahid@gmail.com>
2011-01-15 19:31:29 -0500

    Wrapped the specs in TransitiveBuildr module instead of including the module, since TransitiveBuildr will be included in all spec, even in other files.

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -2,50 +2,52 @@ require 'rubygems'
 require 'buildr'
 require 'lib/transitive-buildr'
 
-include TransitiveBuildr
+module TransitiveBuildr
 
-describe TransitiveBuildr::HelperFunctions do
-  describe 'get_unique_group_artifact' do
-    it 'should return a set of artifacts with version set to 0 given a set of artifacts' do
-      original_set = ['foo:bar:jar:1.0', 'bar:foo:jar:2.0']
-      actual_set = HelperFunctions.get_unique_group_artifact original_set
-      expected_set = ['foo:bar:jar:0', 'bar:foo:jar:0']
-      actual_set.should == expected_set
+  describe TransitiveBuildr::HelperFunctions do
+    describe 'get_unique_group_artifact' do
+      it 'should return a set of artifacts with version set to 0 given a set of artifacts' do
+        original_set = ['foo:bar:jar:1.0', 'bar:foo:jar:2.0']
+        actual_set = HelperFunctions.get_unique_group_artifact original_set
+        expected_set = ['foo:bar:jar:0', 'bar:foo:jar:0']
+        actual_set.should == expected_set
+      end
+
+      it 'should return a array of unique artifacts given a set of artifacts' do
+        original_set = ['foo:bar:jar:1.0', 'foo:bar:jar:2.0']
+        actual_set = HelperFunctions.get_unique_group_artifact original_set
+        expected_set = ['foo:bar:jar:0']
+        actual_set.should == expected_set
+      end
+
     end
 
-    it 'should return a array of unique artifacts given a set of artifacts' do
-      original_set = ['foo:bar:jar:1.0', 'foo:bar:jar:2.0']
-      actual_set = HelperFunctions.get_unique_group_artifact original_set
-      expected_set = ['foo:bar:jar:0']
-      actual_set.should == expected_set
-    end
+    describe 'get_all_versions' do
+      it 'should return all versions in descending order given an artifact and a set of artifacts' do
+        artifact = 'foo:bar:jar:0'
+        original_set = ['foo:bar:jar:1.0', 'foo:bar:jar:2.0']
+        actual_versions = HelperFunctions.get_all_versions artifact, original_set
+        expected_versions = [Version.new('2.0'), Version.new('1.0')]
+        actual_versions.should == expected_versions
+      end
 
+      it 'should return one version only if given an artifact and a set of artifacts with one occurence of the given artifact' do
+        artifact = 'foo:foobar:jar:0'
+        original_set = ['foo:bar:jar:1.0', 'foo:bar:jar:2.0', 'foo:foobar:jar:1.0']
+        actual_versions = HelperFunctions.get_all_versions artifact, original_set
+        expected_versions = [Version.new('1.0')]
+        actual_versions.should == expected_versions
+      end
+
+      it 'should not return repeated versions' do
+        artifact = 'foo:bar:jar:0'
+        original_set = ['foo:bar:jar:1.0', 'foo:bar:jar:1.0']
+        actual_versions = HelperFunctions.get_all_versions artifact, original_set
+        expected_versions = [Version.new('1.0')]
+        actual_versions.should == expected_versions
+      end
+
+    end
   end
 
-  describe 'get_all_versions' do
-    it 'should return all versions in descending order given an artifact and a set of artifacts' do
-      artifact = 'foo:bar:jar:0'
-      original_set = ['foo:bar:jar:1.0', 'foo:bar:jar:2.0']
-      actual_versions = HelperFunctions.get_all_versions artifact, original_set
-      expected_versions = [Version.new('2.0'), Version.new('1.0')]
-      actual_versions.should == expected_versions
-    end
-
-    it 'should return one version only if given an artifact and a set of artifacts with one occurence of the given artifact' do
-      artifact = 'foo:foobar:jar:0'
-      original_set = ['foo:bar:jar:1.0', 'foo:bar:jar:2.0', 'foo:foobar:jar:1.0']
-      actual_versions = HelperFunctions.get_all_versions artifact, original_set
-      expected_versions = [Version.new('1.0')]
-      actual_versions.should == expected_versions
-    end
-
-    it 'should not return repeated versions' do
-      artifact = 'foo:bar:jar:0'
-      original_set = ['foo:bar:jar:1.0', 'foo:bar:jar:1.0']
-      actual_versions = HelperFunctions.get_all_versions artifact, original_set
-      expected_versions = [Version.new('1.0')]
-      actual_versions.should == expected_versions
-    end
-
-  end
 end
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,26 +2,28 @@ require 'rubygems'
 require 'buildr'
 require 'lib/transitive-buildr'
 
-include TransitiveBuildr
+module TransitiveBuildr
 
-describe TransitiveBuildr::Version do
-  it 'should sort versions correctly' do
-    version1 = Version.new '1.2.3'
-    version2 = Version.new '1.2.4'
-    version1.should be < version2
+  describe TransitiveBuildr::Version do
+    it 'should sort versions correctly' do
+      version1 = Version.new '1.2.3'
+      version2 = Version.new '1.2.4'
+      version1.should be < version2
+    end
+
+    it 'should redefine equality as expected' do
+      version1 = Version.new '1.2.3'
+      version2 = Version.new '1.2.3'
+
+      version1.should == version2
+      version1.should eql(version2)
+      version1.hash.should == version2.hash
+    end
+
+    it 'should return the string that was used to initialize it when to_s is called' do
+      version = Version.new '1.2.3'
+      version.to_s.should == '1.2.3'
+    end
   end
 
-  it 'should redefine equality as expected' do
-    version1 = Version.new '1.2.3'
-    version2 = Version.new '1.2.3'
-
-    version1.should == version2
-    version1.should eql(version2)
-    version1.hash.should == version2.hash
-  end
-
-  it 'should return the string that was used to initialize it when to_s is called' do
-    version = Version.new '1.2.3'
-    version.to_s.should == '1.2.3'
-  end
 end
